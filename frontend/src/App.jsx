fb2aa229e751b7b4eee2e9de48125e8e35fde787
SAmmyuxh <samruddhshubhadarshi@gmail.com>
2025-01-29 10:53:41 +0530

    Resolved merge conflict in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Marketplace from "./components/Marketplace";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/marketplace" element={<Marketplace />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </>
   );
